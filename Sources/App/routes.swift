c9105cfbd483aae2c5ac4587ed52bf85fa146d16
lex <akimov.infotech@gmail.com>
2020-11-29 14:54:48 +0600

    New Swift version
    UPdate version

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,36 @@
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req in
+    app.get { refq in
         return "It works!"
     }
 
     app.get("hello") { req -> String in
         return "Hello, world!"
     }
+    app.get("hello", ":name") { req -> String in
+        guard let name = req.parameters.get("name") else {
+            throw Abort(.internalServerError)
+        }
+
+        return "Hello, \(name)!"
+    }
+
+    app.get("hello", "vapor") { req -> String in
+        return "Hello Vapor!"
+    }
+
+    app.post("info") { (req) -> InfoResponse in
+        let data = try req.content.decode(InfoData.self)
+//        return "Hello \(data.name)!"
+        return InfoResponse(request: data)
+    }
+}
+
+struct InfoData: Content {
+    let name: String
+}
+
+struct InfoResponse: Content {
+    let request: InfoData
 }
